78cd71a18206c3975970871ecdafad73bf2e86e4
Eva Rodriguez <eva22rodri@gmail.com>
2019-06-19 13:41:08 -0500

    Grade Distributer using if, if else, and else statements

--- TrueCodeClassNotes/Program.cs
+++ TrueCodeClassNotes/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 
-namespace TrueCodeClassNotes
+namespace ClassNotes
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your grade:");
+            int grade = int.Parse(Console.ReadLine());
+
+            if (grade >= 90)
+            {
+                Console.WriteLine("Your grade is A");
+            }
+            else if (grade >= 80)
+            {
+                Console.WriteLine("Your grade is B");
+            }
+            else if (grade >= 70)
+            {
+                Console.WriteLine("Your grade is C");
+            }
+            else if (grade >= 60)
+            {
+                Console.WriteLine("Your grade D");
+            }
+            else
+            {
+                Console.WriteLine("Your grade F");
+            }
         }
     }
 }
