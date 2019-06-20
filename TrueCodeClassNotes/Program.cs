7d5e12d751caa58df0b972add73cff88b7ad181d
Eva Rodriguez <eva22rodri@gmail.com>
2019-06-20 09:42:10 -0500

    Introduction to Loops

--- TrueCodeClassNotes/Program.cs
+++ TrueCodeClassNotes/Program.cs
@@ -6,29 +6,45 @@ namespace ClassNotes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your grade:");
-            int grade = int.Parse(Console.ReadLine());
+            // Types of loops
+            // An if-statement is not a loop.
 
-            if (grade >= 90)
+            int exampleNumberForWhileLoop = 0;
+
+            //while Loop
+            while (exampleNumberForWhileLoop < 10)
             {
-                Console.WriteLine("Your grade is A");
+                Console.WriteLine("This loop will print this Consol.WriteLine while exampleNumberForWhileLoop is less than ten is true");
+                exampleNumberForWhileLoop++; //We place this here so that the number above does not print infinitly being that we began exampleNumberForWhileLoop as having a static value of Zero. Now it will only print Ten times. 
             }
-            else if (grade >= 80)
+            
+            //Another Example of while loop
+            string userResp = "no";  
+            while (userResp == "no")
             {
-                Console.WriteLine("Your grade is B");
+                Console.WriteLine("Please say yes"); // this can be dont as many times as the user keeps saying no
+                userResp = Console.ReadLine();
             }
-            else if (grade >= 70)
+
+            //Do while loop
+            int numberForDoWhileLoop = 0;
+            do
             {
-                Console.WriteLine("Your grade is C");
-            }
-            else if (grade >= 60)
+                Console.WriteLine("This prints numberForDoWhileLoop once but then does not continue because the number is not less than zero. If it was equal to the conditional in the while (true) then it would keep running till it becomes false.");
+            } while (numberForDoWhileLoop < 0);
+
+            //for loop
+            for (int i = 0; i > 1; i++) // ( We can declare value of I here; this is the conditional true/false; this is the ammount of times for it to increment)
             {
-                Console.WriteLine("Your grade D");
+                Console.WriteLine(i); // for loops do things a specific number of times. 
             }
-            else
+
+            //foreach loop
+            string[] names = { "Chip", "Zee", "Dan", "Brandon" };//names is the name of the collection
+            foreach (string name in names) // for every one of the strings inside of the collection we are going to call it name.
             {
-                Console.WriteLine("Your grade F");
-            }
+                Console.WriteLine(name); // this will print the collection of strings so all the names will appear on the Console.
+            } //name declares a string for each item inside the this loop // example using real life situation.. for each email in emails send marketing email.
         }
     }
 }
