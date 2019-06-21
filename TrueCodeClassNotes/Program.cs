6fd16c754e8dae5c01f52eca26fbffa1cc422027
Eva Rodriguez <eva22rodri@gmail.com>
2019-06-21 13:32:53 -0500

    Review on Loops , and added a feature on slowing down the speed of printing info through console.

--- TrueCodeClassNotes/Program.cs
+++ TrueCodeClassNotes/Program.cs
@@ -6,13 +6,35 @@ namespace ClassNotes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your favorite number?");
 
-            string response = Console.ReadLine();
+            // KINDS OF LOOPs
 
-            int favoriteNumber = int.Parse(response); //parce method takes the string entered and transforms it into the integer that is equal to that string.
+            //for loop
+            for(int i = 1; i < 100; i++)
+            {
+                System.Threading.Thread.Sleep(1000); // this is not needed for the loop, it is extra just to show how to slow down how fast console prints.
+                Console.WriteLine(i + 1); ///"This will print the number ONE through ONE HUNDRED"
+            }
 
-            Console.WriteLine("Your favorite number is " + favoriteNumber);
+            //foreach
+            int[] numbers = { 4, 7, 3, 2, 5, 1, 9 }; // collection of numbers
+            foreach(int xyz in numbers)
+            {
+                Console.WriteLine(xyz);
+            }
+
+            //while loop '
+            bool playerIsAlive = true;
+            while (playerIsAlive == true)// great as long as something is true
+            {
+                // play the game
+            }
+
+            //do while loop
+            do
+            {
+                // This will always run at else once and depending on the while, it may continue to run.
+            } while (2 == 1);
         }
     }
 }
