a87746e12fdc7483197131b05f5e0acb2ccf79c5
Eva Rodriguez <eva22rodri@gmail.com>
2019-06-24 13:21:48 -0500

    FavoriteNumber: Get user favorite numbers before using methods

--- TrueCodeClassNotes/Program.cs
+++ TrueCodeClassNotes/Program.cs
@@ -6,49 +6,22 @@ namespace ClassNotes
     {
         static void Main(string[] args)
         {
-            for(int i = 1; i <= 10; i++)
-            {
-                DisplayOddsFrom1ToN(i); //same as code vvv
-            }
+            Console.WriteLine("Let's add 4 numbers.");
 
-            // This does the same thing as the method above
-            //DisplayOddsFrom1ToN(1);
-            //DisplayOddsFrom1ToN(2);
-            //DisplayOddsFrom1ToN(3);
-            //DisplayOddsFrom1ToN(4);
-            //DisplayOddsFrom1ToN(5);
-            //DisplayOddsFrom1ToN(6);
-            //DisplayOddsFrom1ToN(7);
-        }
+            Console.WriteLine("What is number 1?");
+            string response1 = Console.ReadLine();
+            int number1 = int.Parse(response1);
 
-        static void DisplayOddsFrom1ToN(int n)
-        {
-            for (int i = 1; i < n; i ++)
-            {
-                if(i % 2 != 0) 
-                {
-                   Console.Write(i + " "); // this prints ( 1 3 5 7 9 11 13 15 17..etc..) HORIZONTALLY
-                }
-            }
-            Console.WriteLine();
-            
+            Console.WriteLine("What is number 2?");
+            string response2 = Console.ReadLine();
+            int number2 = int.Parse(response1);
+
+            Console.WriteLine("What is number 3?");
+            string response3 = Console.ReadLine();
+            int number3 = int.Parse(response1);
+
+            int total = number1 + number2 + number3;
+            Console.WriteLine("The total is " + total);
         }
     }
 }
-
-
-//**** Other Notes  ****
-
-//static void DisplayOddsFrom1To100()
-//{
-//for (int i = 1; i < 100; i += 2)
-//{
-//    if (i % 2 != 0) // (  %  ) runs modulous aka the remainder when divided by 2. 0 means it is even.
-//    {
-//        // Console.Write(i); // this prints ( 1357911131517..etc..)
-//        Console.Write(i + " "); // this prints ( 1 3 5 7 9 11 13 15 17..etc..) HORIZONTALLY.
-//                                //Console.WriteLine(i); // this prints vertically, single values.
-//                                //dsaf
-//    }
-//}
-//}
