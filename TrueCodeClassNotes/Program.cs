e8a315ffc70b1f88b9b4cbb2b1b9b84057dd8a12
Eva Rodriguez <eva22rodri@gmail.com>
2019-06-24 13:31:34 -0500

    FavoriteNumber: refactored... method adds all four numbers

--- TrueCodeClassNotes/Program.cs
+++ TrueCodeClassNotes/Program.cs
@@ -8,20 +8,22 @@ namespace ClassNotes
         {
             Console.WriteLine("Let's add 4 numbers.");
 
-            Console.WriteLine("What is number 1?");
-            string response1 = Console.ReadLine();
-            int number1 = int.Parse(response1);
+            int number1 = GetIntegerFromUser("What is number 1");
+            int number2 = GetIntegerFromUser("What is number 2");
+            int number3 = GetIntegerFromUser("What is number 3");
+            int number4 = GetIntegerFromUser("What is number 4?");
 
-            Console.WriteLine("What is number 2?");
-            string response2 = Console.ReadLine();
-            int number2 = int.Parse(response1);
+            int total = number1 + number2 + number3 + number4;
 
-            Console.WriteLine("What is number 3?");
-            string response3 = Console.ReadLine();
-            int number3 = int.Parse(response1);
-
-            int total = number1 + number2 + number3;
             Console.WriteLine("The total is " + total);
+        }
+
+        static int GetIntegerFromUser(string prompt)
+        {
+            Console.WriteLine(prompt);
+            string response1 = Console.ReadLine();
+            int number = int.Parse(response1);
+            return number;
         }
     }
 }
