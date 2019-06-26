f2a9e7998a66985615647f3c0fc28516cc684413
Eva Rodriguez <eva22rodri@gmail.com>
2019-06-26 13:52:03 -0500

    NumberGuessingGame Pt4: Fixed exceptions when non-int answers are written, added a point system

--- TrueCodeClassNotes/Program.cs
+++ TrueCodeClassNotes/Program.cs
@@ -4,53 +4,53 @@ namespace ClassNotes
 {
     class Program
     {
-        static void Main(string[] args)
+        static int GetIntegerFromUser(string question) //Give a question and this method will do the following.
         {
-            Console.WriteLine("Let's play a number guessing game!");
-            
-            // loop to make sure that the input is an integer
-            int maxValueWanted;
+            int integerFromUser;
             bool success;
             do
             {
                 Console.WriteLine("Enter the max for the range you'd like to play with:");
                 string strMax = Console.ReadLine();
-                success = int.TryParse(strMax, out maxValueWanted);
+                success = int.TryParse(strMax, out integerFromUser);
             } while (success == false);
 
-            Random rnd = new Random();
-            int secretNumber = rnd.Next(1, maxValueWanted + 1);
+            return integerFromUser;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's play a number guessing game!");
 
+            int max = GetIntegerFromUser("Enter the max for the range you'd like to play with:");
+
+            Random rnd = new Random();
+            int secretNumber = rnd.Next(1, max + 1);
+
+            int score = 0;
             int guess;
             do
             {
-                Console.ForegroundColor = ConsoleColor.White;
+                Console.ResetColor();
 
-                bool successfulGuess;
-                do
-                {
-                    Console.WriteLine("Please guess a number between 1 - " + maxValueWanted + ": ");
-                    string strGuess = Console.ReadLine();
-                    successfulGuess = int.TryParse(strGuess, out guess);
-                } while (successfulGuess == false);
+                Console.WriteLine("Your current score is " + score);
+                guess = GetIntegerFromUser("Please guess a number between 1 - " + max + ": ");
 
-
-                if (guess == secretNumber)
-                {
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.WriteLine("You got it!");
-                }
-                else if(guess > secretNumber)
+                if (guess > secretNumber)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("You were too high, loser! Guess again..");
+                    score += 1;
                 }
-                else
+                else if (guess < secretNumber)
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine("You were too low, loser! Guess again..");
+                    score += 1;
                 }
             } while (guess != secretNumber);
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("You got it!");
         }
     }
 }
