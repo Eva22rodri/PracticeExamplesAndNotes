713cb2d236593b6d2663ce75961a114a82bc495c
Eva Rodriguez <eva22rodri@gmail.com>
2019-06-27 14:00:38 -0500

    LISTs vs ARRAYs: Initial Notes

--- TrueCodeClassNotes/Program.cs
+++ TrueCodeClassNotes/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic; //needed to be able to use a LIST
+
 
 namespace ClassNotes
 {
@@ -6,25 +8,18 @@ namespace ClassNotes
     {
         static void Main(string[] args)
         {
-            //****  Arrays ****
+            //Arrays size (length) is fixed
+            string[] namesArray = new string[3]; //length of three means there are 2 indexes
+            namesArray[0] = "Alan";
+            namesArray[1] = "Bety";
+            namesArray[2] = "Shay";
 
-            // The size of an array is set once established it can not be grown nor made smaller. Size is fixed once created.
-            string[] names = ["Ted", "Jill", "Joe"]; // this is an array of names.
-            string name1 = names[0]; // this is telling which name i want 
-            names[0] = "Jane"; // this will change the name at index Zero to Jane.
-
-            // making a shift to make each name in names its own individual string.
-            // note: almost always working with individual elevments, not the entire array. ex: ted... or jill... not ted jill and joe.
-
-            for(int i = 0; i < names.Length; i++)
-            {
-                Console.WriteLine(names[i]); // this will print whatever string is at the 'i' value. 
-            }
-
-            foreach (string name in names)
-            {
-                Console.WriteLine(name);  // does the same thing as for loop above. 
-            }
+            //Lists size (count) can change
+            List<string> namesList = new List<string>();
+            namesList.Add("Alan");
+            namesList.Add("Bety");
+            namesList.Add("Shay");
         }
+
     }
 }
