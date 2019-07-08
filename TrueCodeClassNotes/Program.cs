bf44200b9ec03a28c7947aad9cf2c8683ff5b56c
Eva Rodriguez <eva22rodri@gmail.com>
2019-07-08 10:16:02 -0500

    Api pt2: Added Nuget Packet and simplified

--- TrueCodeClassNotes/Program.cs
+++ TrueCodeClassNotes/Program.cs
@@ -1,21 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using Newtonsoft.Json.Linq;
 
-
+// Added a NuGet Package  "NewtonSoft.Json"
 namespace ClassNotes
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            string url = "https://api.chucknorris.io/jokes/random"; //Chuck Norris API website.
+            //This is not best practice, but it demonstrates making an API
+            //call and parseing a Json response
+
+            string url = "https://api.chucknorris.io/jokes/random"; 
             HttpClient client = new HttpClient();          
             string response = client.GetStringAsync(url).Result;
 
-            int jokeStartIndex = response.IndexOf("value") + 8; //The joke is going to start at the word value
-            int jokeLength = response.Length - jokeStartIndex - 2;
-            string joke = response.Substring(jokeStartIndex, jokeLength);
+           
+
+            string joke = JObject.Parse(response).GetValue("value").ToString();
 
             Console.WriteLine(joke);
         }    
